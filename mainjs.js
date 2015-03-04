b6a9c2ac2814515d7a868b084f6dff782f1d5d92
int-argc <jeffrey_wong@dlsu.edu.ph>
2015-03-04 09:32:38 +0800

    Basic functions are working

--- mainjs.js
+++ mainjs.js
@@ -1,26 +1,58 @@
 // mainjs.js
 
+var dataset;
 
-function getMedian() {
-	var x = median();
-	document.getElementById("answer").innerHTML = "Median: " + x;
+$(document).ready(function() {
+	// initialize
+	$("#dataset").linedtextarea();
+	
+	$("#calcMean").click(function() {
+		output(jStat.mean(getInput()));
+	});
+	
+	$("#calcMedian").click(function() {
+		output(jStat.median(getInput()));
+	});
+	
+	$("#calcMode").click(function() {
+		output(jStat.mode(getInput()));
+	});
+	
+	$("#calcStdDev").click(function() {
+		output(jStat.stdev(getInput()));
+	});
+	
+	$("#calcVariance").click(function() {
+		output(jStat.variance(getInput()));
+	});
+	
+	$("#calcRange").click(function() {
+		output(jStat.range(getInput()));
+	});
+	
+	$("#calcSkewness").click(function() {
+		output(jStat.skewness(getInput()));
+	});
+	
+	$("#calcCoeffvar").click(function() {
+		output(jStat.coeffvar(getInput()));
+	});
+	
+	$("#calcKurtosis").click(function() {
+		output(jStat.kurtosis(getInput()));
+	});
+});	// end of document.ready
+
+function output(out) {
+	document.getElementById("answer").innerHTML = out;
 }
 
-
-function median() {
-	var arr = getInput();
-	arr.sort(function(a, b){return a - b});
-	var mid = Math.floor(arr.length / 2);
-	if(arr.length % 2 == 0) {
-		return (parseInt(arr[mid - 1]) + parseInt(arr[mid])) / 2;
-	}
-	else
-		return arr[mid];
-}
-
+// implement error checking here
 function getInput() {
-	return document.getElementById("numbers").value.split(" ");
+	var arr = new Array();
+	var strArr = document.getElementById("dataset").value.split('\n');	// di ko alam ano yung whitespace na regex
+	for(var i = 0; i < strArr.length; i++)
+		arr[i] = parseInt(strArr[i]);
+	return arr;
 }
-
-
 
